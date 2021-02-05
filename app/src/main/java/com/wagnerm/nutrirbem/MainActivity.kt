747ccdbee936f4445a906d77491f7bf0d3fa096f
wagnerMascarenhas <wagnerMascarenhas@users.noreply.github.com>
2021-02-05 17:46:33 -0300

    V0.1.3
    Mudança de tela - OK
    Tela Entrada -> Tela Login

--- app/src/main/java/com/wagnerm/nutrirbem/MainActivity.kt
+++ app/src/main/java/com/wagnerm/nutrirbem/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.wagnerm.nutrirbem
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.*
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.wagnerm.nutrirbem.ui.login.LoginActivity
 import kotlinx.coroutines.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         GlobalScope.launch { // launch a new coroutine in background and continue
-            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-            val intent = Intent(MainActivity.class,LoginActivity.class)
-            startActivity(intent)
+            delay(3000L) // non-blocking delay for 1 second (default time unit is ms)
+            val it = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(it)
         }
     }
 }
