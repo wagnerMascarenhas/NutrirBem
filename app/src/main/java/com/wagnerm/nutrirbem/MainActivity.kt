c6d5bf8661ae4f7e7aafb2b8ce83c2c6b1c756ce
wagnerMascarenhas <wagnerMascarenhas@users.noreply.github.com>
2021-02-05 13:02:35 -0300

    V0.1.2

--- app/src/main/java/com/wagnerm/nutrirbem/MainActivity.kt
+++ app/src/main/java/com/wagnerm/nutrirbem/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.wagnerm.nutrirbem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wagnerm.nutrirbem.ui.login.LoginActivity
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch { // launch a new coroutine in background and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+            val intent = Intent(MainActivity.class,LoginActivity.class)
+            startActivity(intent)
+        }
     }
 }
